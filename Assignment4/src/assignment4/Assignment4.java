0708ee42429bdf3167fc7b4aef90a902e32163ed
K00223375 <K00223375@student.lit.ie>
2016-11-28 10:04:17 +0000

    Typing App first push
    Interactive keyboard almost completed.

---
+++ Assignment4/src/assignment4/Assignment4.java
@@ -0,0 +1,16 @@
+package assignment4;
+
+/**
+ *
+ * @author K00223375
+ */
+public class Assignment4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
