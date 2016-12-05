f1fffc7bc697c6709d6e86006343ab13e5f88acd
K00223375 <K00223375@student.lit.ie>
2016-12-05 09:41:44 +0000

    Temp files and build removed

--- Assignment4/src/assignment4/Assignment4.java
+++ Assignment4/src/assignment4/Assignment4.java
@@ -1,21 +1,18 @@
+
 package assignment4;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
+
 /**
  *
- * @author K00223375
+ * @author Alan Stewart K00223375
  */
 public class Assignment4 {
-    private static TypingTutor typingApp= new TypingTutor();
-    private String path;
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) 
     {
-        
+        TypingTutor typingApp = new TypingTutor();
     }
- 
-    public void writeToFile(String x, String y) throws IOException
-    {
-        FileWriter write = new FileWriter(path,true);
-    }
+    
 }
